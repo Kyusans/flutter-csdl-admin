e3be4d1b07890c5cfc47818ec30dbd6e35a5e996
Mel Macario <xmelmacario@gmail.com>
2024-01-25 20:55:06 +0800

    added localstorage functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_csdl_admin/local_storage.dart';
-import 'package:flutter_csdl_admin/pages/add_scholar.dart';
 import 'package:flutter_csdl_admin/pages/dashboard.dart';
 // import 'package:flutter_csdl_admin/pages/dashboard.dart';
 import 'package:flutter_csdl_admin/pages/login_page.dart';
 import 'package:flutter_csdl_admin/theme/dark_mode.dart';
 import 'package:flutter_csdl_admin/theme/light_mode.dart';
+import 'package:get/get.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,13 +19,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         '/login': (context) => const LoginPage(),
         '/dashboard': (context) => const Dashboard(),
-        '/addScholar': (context) => const AddScholar(),
       },
-      debugShowCheckedModeBanner: false,
       home: LoginPage(),
       //LoginPage()
       theme: lightMode,
